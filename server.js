27be54a69b10c741b86626864da07e327cc79def
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-20 22:41:32 +0530

    commit

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./env.config" });
-// console.log(process.env);
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
+// ;
 const server = require("./app");
+// console.log(process.env.DATABASE);
+const DB = process.env.DATABASE
+// DB.replace()
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    user: process.env.userName, pass: process.env.userPass
+  })
+  .then(() => {
+    console.log("connection successful");
+  });
 
 server.listen(8000, (err) => {
   if (err) {
