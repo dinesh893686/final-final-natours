734a7000916430660e50ea2eb6db72859281f273
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-21 12:27:16 +0530

    commit

--- dev-data/data/importData.js
+++ dev-data/data/importData.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv");
-const fs=require('fs')
+const fs = require('fs')
 const mongoose = require("mongoose");
 dotenv.config({ path: "./../../config.env" });
-const Tour=require(`${__dirname}/../../models/tourModel`)
+const Tour = require(`${__dirname}/../../models/tourModel`)
 // ;
 // const server = require("./app");
 // console.log(process.env.DATABASE);
@@ -12,95 +12,71 @@ const DB = process.env.DATABASE
 
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    user: process.env.userName, pass: process.env.userPass
-  })
-  .then(() => {
-    console.log("connection successful");
-  });
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        user: process.env.userName, pass: process.env.userPass
+    })
+    .then(() => {
+        console.log("connection successful");
+    });
 
-const tours=JSON.parse(fs.readFileSync(`${__dirname}/tour-simple.json`,'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tour-simple.json`, 'utf-8'))
 //import data into db
-const importData=async()=>{
-    try{
- const tours=await Tour.create(tours)
-   res.status(200).json(
-       {
-         message:"success",
-          data:{
-              tours
-          }
-       }
-   )
+const importData = async () => {
+    try {
+        const tours = await Tour.create(tours)
+        console.log('created successfully')
 
 
     }
-    catch{  (err)=>
-        {
-        res.status(404).json(
-            {
-              message:"fail",
-            //    data:{
-            //        tours
-            //    }
-            error:err
-            }
-        )
-        }
-     
-     
+    catch (err) {
+        console.log(err)
 
     }
+
+    process.exit()
+
+
 
 }
 //delete data from db
-const deleteData=async()=>{
+const deleteData = async () => {
 
 
-    try{
-await Tour.deleteMany()
-   res.status(200).json(
-       {
-         message:"success",
-          
-       }
-   )
+    try {
+        await Tour.deleteMany()
+        console.log('Data successfully Deleted')
+
+
 
 
     }
-    catch{(err)=>{
-        res.status(404).json(
-            {
-              message:"fail",
-            //    data:{
-            //        tours
-            //    }
-            err
-            }
-        )
-        }
-     
+    catch (err) {
+        console.log(err)
 
     }
 
 
 
+    process.exit()
 
 
+
+
+
 }
-if(process.argv[2]=='--import')
-{
-    importData()
-}
-else if(process.argv[2]=='--delete')
-{
-   deleteData()
-}
-    
-  
+// if(process.argv[2]=='--import')
+// {
+//}
+// else if(process.argv[2]=='--delete')
+// {
+deleteData
+importData
+// }
+
+
 
 
