2b4212004648115e6f6c50dd92f3ef95b347bd88
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-22 20:53:40 +0530

    commit

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,54 +1,74 @@
-// const fs = require("fs");
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, "utf-8")
-// );
+
 
 const { findByIdAndUpdate } = require('./../models/tourModel');
 const Tour = require('./../models/tourModel')
-// exports.checkId = (req, res, next, val) => {
-//   if (val > tours.length) {
-//     return res.status(404).json({
-//       messsage: "not found",
-//     });
-//   }
-//   next();
-// };
-// exports.checkBody = (req, res, next) => {
-//   const name = req.body.name;
-//   const price = req.body.price;
-//   if (!name || !price) {
-//     return res.status(404).json({
-//       message: "invalid input",
-//     });
-//   }
-//   next();
-// };
+
+const apiFeatures = require('./../utils/apiFeatures')
+
+exports.aliasTopTour = (req, res, next) => {
+  req.query.limit = '5',
+    req.query.fields = 'name price duration'
+  next()
+}
+
 exports.getALLTour = async (req, res) => {
-  // console.log(req.requestTime);
+
   try {
 
-    const query_obj = { ...req.query }
-    const excluded_query = ['page', 'sort', 'limit', 'fields']
-    excluded_query.forEach((el) => {
-      delete query_obj[el]
+    // console.log(req.query)
 
-    })
-    // const id = query_obj * 1
-    console.log(query_obj)
+    const x = new apiFeatures(Tour.find(), req.query).filter().sort().limitFields().pagination();
+    const query = x.query
+    //filter
+    // const query_obj = { ...req.query }
+    // const excluded_query = ['page', 'sort', 'limit', 'fields']
+    // excluded_query.forEach((el) => {
+    //   delete query_obj[el]
 
-    let query_str = JSON.stringify(query_obj)
-    // console.log(query_str)
-    query_str = query_str.replace(/\(gte|gt|lte|lt)\b/g, (match) => `$${match}`
+    // })
 
-    )
-    console.log(JSON.parse(query_str))
-    const query = Tour.find(JSON.parse(query_str))
+    // let query_str = JSON.stringify(query_obj)
 
+    // query_str = query_str.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`
+    // )
+
+    // let query = Tour.find(JSON.parse(query_str))
+
+
+    //sort 
+    // if (req.query.sort) {
+    //   let sortBy = req.query.sort.split(',').join(' ')
+    //   query = query.sort(sortBy)
+    // }
+
+    //limitFields
+    // if (req.query.fields) {
+    //   let limitBy = req.query.fields.split(',').join(' ')
+    //   query = query.select(limitBy)
+    // }
+    // else {
+    //   query = query.select('-__v')
+    // }
+
+    //pagination
+    // const page = req.query.page * 1 || 1
+    // const limit = req.query.limit * 1 || 100
+    // const skip_page = (page - 1) * limit
+    // // if (req.query.page) {
+    // if (req.query.page) {
+    //   if (skip_page >= await Tour.countDocuments) {
+    //     throw new Error('This page does not exist')
+    //   }
+    // }
+
+    // else {
+    //   query = query.skip(skip_page).limit(limit)
+    // }
     const tours = await query
 
     res.status(200).json({
       messsage: "success",
-
+      length: tours.length,
       data: {
         tours,
       },
@@ -90,9 +110,7 @@ exports.createTour = async (req, res) => {
 };
 
 exports.getTour = async (req, res) => {
-  //   console.log(req.params);
-  // const x = req.params.id * 1;
-  // const tour = tours.find((el) => el.id == x);
+
   try {
     const tour = await Tour.findById(req.params.id)
     res.status(200).json({
