91e50e6a47494b2423b10d6f2fed50af0f2817e1
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-24 18:39:20 +0530

    c

--- app.js
+++ app.js
@@ -29,11 +29,35 @@ app.all("*", (req, res, next) => {
 //handling cast error
 
 
-const handleCastError = (err, res) => {
+const handleCastError = (err) => {
 
   const message = `you have entered wrong ${err.path} with value ${err.value}`;
 
   return new AppError(message, 404)
+
+
+
+}
+const handleDuplicationKeyError = (err) => {
+
+  const message = `you have entered duplicate key name as  ${err.keyValue.name} `;
+  return new AppError(message, 404)
+
+}
+
+const handleValidationError = (err) => {
+  const errors = Object.values(err.errors).map(el =>
+
+    el.message
+
+  )
+  const message = `invalid input ${errors.join('. ')}`
+
+  return new AppError(message, 404)
+
+
+
+
 
 
 
@@ -64,7 +88,8 @@ const errInProduction = (err, res) => {
   // err like syntax mistkes or express error 
   else {
     console.log(err);
-    res.status().json({
+    // console.log(err.message);
+    res.json({
       message: "Something happened very wrong!"
     })
   }
@@ -72,6 +97,7 @@ const errInProduction = (err, res) => {
 }
 
 app.use((err, req, res, next) => {
+  console.log(err)
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
   if (process.env.NODE_ENV === "development") {
@@ -84,7 +110,13 @@ app.use((err, req, res, next) => {
       Error = handleCastError(Error);
     }
 
+    else if (Error.code === 11000) {
+      Error = handleDuplicationKeyError(Error)
+    }
 
+    else if (Error._message === 'Tour validation failed') {
+      Error = handleValidationError(Error)
+    }
     errInProduction(Error, res)
   }
 });
