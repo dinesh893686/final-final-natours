8d451174d16c8e7b5e57c4904f54df6e9ad6cd75
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-24 20:38:20 +0530

    c

---
+++ controller/authController.js
@@ -0,0 +1,25 @@
+
+
+const User = require('./../models/userModel')
+
+const catchAsync = require('./../utils/catchAsync')
+
+
+exports.signup = catchAsync(async (req, res) => {
+
+    const user = await User.create(req.body)
+    res.status(200).json({
+
+        status: "success",
+        data: {
+            user
+
+        }
+
+
+    })
+
+
+
+}
+)
