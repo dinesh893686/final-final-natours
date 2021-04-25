84416aacc70589ea8e2c0f5af2ae3d21af0eb3e8
Dinesh Shah <dineshshah8234@gmail.com>
2021-04-25 11:04:20 +0530

    c

--- controller/authController.js
+++ controller/authController.js
@@ -3,14 +3,18 @@
 const User = require('./../models/userModel')
 
 const catchAsync = require('./../utils/catchAsync')
-
+const jwt = require('jsonwebtoken')
 
 exports.signup = catchAsync(async (req, res) => {
 
     const user = await User.create(req.body)
+
+
+    const token = jwt.sign({ id: user._id }, process.env.jwt_secreat)
     res.status(200).json({
 
         status: "success",
+        token,
         data: {
             user
 
@@ -23,3 +27,8 @@ exports.signup = catchAsync(async (req, res) => {
 
 }
 )
+
+
+
+// exports.
+
